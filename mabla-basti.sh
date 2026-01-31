8f5bfc893178612645877b33508eb44a98d45233
la .varik. .VALefor. <varikvalefor@aol.com>
2026-01-31 16:30:46 -0500

    ni'o tolsti tu'a le fanva ke proga co'e

---
+++ mabla-basti.sh
@@ -0,0 +1,7 @@
+#!/bin/ksh
+
+# ni'o ko'a goi le proga co'e cu jai filiri'a lo nu fanva fi le glibau kei ni'i zo'e joi le su'u vo'a zmiku lo nu basti pe'a
+#
+# .i ku'i jitfa fa le du'u ko'a mapti lo ro co'e  .i troci zo'e naje lo nu ko'a mapti lo se me'oi .kibykaerni.
+
+sed -e 's/* le ka ce'u mutce le ka ko'a goi ce'u xi re gleki le su'u ko'a nonlingeni je le su'u ko'a jimpe le du'u ko'a nonlingeni kei kei je le su'u le ka ce'u xi ci nonlingeni cu jinzi ko'a gi ga ce/* that (VARIK is extreme in being glad about that (VARIK is agender), that (VARIK understands that VARIK is agender), and that (being agender is inherent to VARIK)),/'
