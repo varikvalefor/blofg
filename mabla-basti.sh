62072157de6d34d328656f1176d8427861263c28
la .varik. .VALefor. <varikvalefor@aol.com>
2026-02-03 19:49:05 -0500

    ni'o zmadu cusku

--- mabla-basti.sh
+++ mabla-basti.sh
@@ -3,6 +3,8 @@
 # ni'o ko'a goi le proga co'e cu jai filiri'a lo nu fanva fi le glibau kei ni'i zo'e joi le su'u vo'a zmiku lo nu basti pe'a
 #
 # .i ku'i jitfa fa le du'u ko'a mapti lo ro co'e  .i troci zo'e naje lo nu ko'a mapti lo se me'oi .kibykaerni.
+#
+# .i ku'i filri'a tu'a lo jai cafne
 
 sed -e "s/\\* le ka ce'u mutce le ka ko'a goi ce'u xi re gleki le su'u ko'a nonlingeni je le su'u ko'a jimpe le du'u ko'a nonlingeni kei kei je le su'u le ka ce'u xi ci nonlingeni cu jinzi ko'a gi ga ce/* that (VARIK is extreme in being glad about that (VARIK is agender), that (VARIK understands that VARIK is agender), and that (being agender is inherent to VARIK)),/" |\
 sed -e "s/\\* le ka ce'u gleki le su'u ri ckaji le ka ce'u xi re tonsi noi ku'i ke'a jinzi ga naje ce'u gi ri joi le xadni be ri gi ga ce/* that (VARIK is glad about that (VARIK is transgender) (which is not inherent to VARIK but is inherent to the combination of VARIK and the body of VARIK)),/" |\
